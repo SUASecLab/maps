0f01917094d9213200db784835d3ec9b313dab78
Tobias Tefke <tobias@tefke.name>
2025-08-06 10:28:49 +0200

    Tips: add light/dark mode switch

--- tips_src/src/js/main.js
+++ tips_src/src/js/main.js
@@ -3,3 +3,46 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap’s JS
 import * as bootstrap from 'bootstrap'
+
+// Dark/light mode
+// Get currently stored theme
+let theme = localStorage.getItem("theme");
+
+// No or invalid theme stored
+if ((theme == null) || ((theme != "light") && (theme != "dark"))) {
+    theme = "dark"
+    window.localStorage.setItem("theme", theme)
+}
+
+// Get theme switch button
+const themeButton = document.getElementById("theme-switch");
+
+// Enable dark mode function
+function enableDarkMode() {
+    document.documentElement.setAttribute("data-bs-theme", "dark");
+    window.localStorage.setItem("theme", "dark");
+    themeButton.innerText = "Enable light mode";
+}
+
+// Enable light mode function
+function enableLightMode() {
+    document.documentElement.setAttribute("data-bs-theme", "light");
+    window.localStorage.setItem("theme", "light");
+    themeButton.innerText = "Enable dark mode";
+}
+
+// Set theme
+if (theme == "dark") {
+    enableDarkMode();
+} else {
+    enableLightMode();
+}
+
+// Change theme button
+themeButton.addEventListener("click", () => {
+    if (document.documentElement.getAttribute("data-bs-theme") == "light") {
+        enableDarkMode();
+    } else {
+        enableLightMode();
+    }
+});
